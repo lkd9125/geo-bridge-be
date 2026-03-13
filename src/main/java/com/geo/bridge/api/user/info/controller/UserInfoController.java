8248c433fc795a3ed17ee9171e38886e82907c4e
이경도 <lkd9125@palnet.co.kr>
2026-03-13 17:28:43 +0900

    refactor: model 필드 추가

--- src/main/java/com/geo/bridge/api/user/info/controller/UserInfoController.java
+++ src/main/java/com/geo/bridge/api/user/info/controller/UserInfoController.java
@@ -1,6 +1,5 @@
 package com.geo.bridge.api.user.info.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/geo/bridge/api/user/info/model/GetUserRS.java
+++ src/main/java/com/geo/bridge/api/user/info/model/GetUserRS.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class GetUserRS {
 
-    
+    private String id;
 
 }
