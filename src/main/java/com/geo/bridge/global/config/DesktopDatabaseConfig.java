e80762c22d450e53fdb0e33f00baa5e4767e571f
이경도 <lkd9125@palnet.co.kr>
2026-03-30 10:26:26 +0900

    refactor: desktop scheme init 설정 추가가

---
+++ src/main/java/com/geo/bridge/global/config/DesktopDatabaseConfig.java
@@ -0,0 +1,36 @@
+package com.geo.bridge.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import io.r2dbc.spi.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Profile("desktop")
+@Slf4j
+@Lazy(false)
+public class DesktopDatabaseConfig {
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        log.info("음하하");
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(
+            new ResourceDatabasePopulator(new ClassPathResource("db/h2/ddl.sql"))
+        );
+        initializer.setDatabasePopulator(populator);
+        
+        return initializer;
+    }
+
+}
